b38c638f7f0710bb4dba61c2d7fa85af2a778978
KarampelasDS <24dimitris@gmail.com>
2026-01-30 21:32:26 +0200

    Added the --verbose argument

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,11 +14,20 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Chabot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose",action="store_true",help="Enable verbose output")
     args = parser.parse_args()
-    response = client.models.generate_content(model="gemini-2.5-flash",contents=args.user_prompt)
+    
+    messages = [types.Content(role="user",parts=[types.Part(text=args.user_prompt)])]
+    
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+    
+    if args.verbose:
+        print(f"User prompt:{args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    
     if response.usage_metadata is not None:
-        print("Prompt tokens: ",response.usage_metadata.prompt_token_count)
-        print("Response tokens: ",response.usage_metadata.candidates_token_count)
         print(response.text)
     else:
         raise RuntimeError("No usage metadata found in response")
